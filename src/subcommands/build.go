1b55fea90fa4e2235e0da91be34bc090bcc5d665
vaelixd <vaelixd@proton.me>
2025-11-30 21:33:55 +1300

    add more control over user generation and implement package installation

--- src/subcommands/build.go
+++ src/subcommands/build.go
@@ -2,6 +2,7 @@ package subcommands
 
 import (
 	"fmt"
+	"maps"
 	"os"
 	"path/filepath"
 	"slices"
@@ -10,13 +11,15 @@ import (
 
 	"github.com/Songbird-Project/nest/core"
 	"github.com/Songbird-Project/nest/scripting"
+	"github.com/Songbird-Project/scsv"
 	"github.com/charmbracelet/lipgloss/v2"
 )
 
 type BuildCmd struct {
-	Name       string `arg:"-n,--name" help:"name the new build generation rather than using a number"`
-	NoCompress bool   `arg:"--no-compress" help:"do not compress the previous generation"`
-	Home       bool   `arg:"-H,--home" help:"only rebuild managed home directories rather than the whole system"`
+	Name               string `arg:"-n,--name" help:"name the new build generation rather than using a number"`
+	NoCompress         bool   `arg:"--no-compress" help:"do not compress the previous generation"`
+	IgnoreExistingHome bool   `arg:"-i,--ignore-existing-home" help:"do not restore the previous home directory of a previously existing user that has been recreated"`
+	Home               bool   `arg:"-H,--home" help:"only rebuild managed home directories rather than the whole system"`
 }
 
 func SysBuild(args *BuildCmd) error {
@@ -75,67 +78,12 @@ func SysBuildAll(args *BuildCmd, pm core.PrivilegeManager) error {
 	}
 
 	if os.Getenv("NEST_DRY_RUN") == "" {
-		fmt.Println(infoStyle.Render("Adding users:"))
-
-		users, err := core.GetUserConfigs()
-		if err != nil {
+		if err := manageUsers(pm); err != nil {
 			return err
 		}
 
-		userList, homes, err := core.GetUsers()
-		if err != nil {
+		if err := installPkgs(pm); err != nil {
 			return err
-		}
-
-		var cfgUsers []string
-
-		for _, userCfg := range users {
-			cfgUsers = append(cfgUsers, userCfg.Username)
-
-			if !slices.Contains(userList, userCfg.Username) {
-				fmt.Println(infoStyle.Render(" - ", userCfg.Fullname))
-
-				args := []string{
-					"-d",
-					userCfg.HomeDir,
-					"-c",
-					userCfg.Fullname,
-					"-s",
-					userCfg.Shell,
-				}
-
-				homeDirArchive := strings.TrimSuffix(userCfg.HomeDir, "/") + ".tar.zst"
-
-				existingHome, err := core.PathExists(
-					homeDirArchive,
-				)
-				if err != nil {
-					return err
-				}
-
-				if existingHome {
-					pm.RunAsAuthUser("tar", []string{"--zstd", "-xvf", homeDirArchive})
-				} else {
-					args = append(args, "-m")
-				}
-
-				if len(userCfg.Groups) > 0 {
-					args = append(args, "-G", strings.Join(userCfg.Groups, ","))
-				}
-
-				args = append(args, userCfg.Username)
-
-				if err := pm.RunAsAuthUser("useradd", args); err != nil {
-					return err
-				}
-			}
-		}
-
-		for idx, existingUser := range userList {
-			if !slices.Contains(cfgUsers, existingUser) {
-				pm.RunAsAuthUser("userdel", []string{existingUser})
-				archiveAndRemove(homes[idx], pm)
-			}
 		}
 	} else {
 		fmt.Println("Destructive Build")
@@ -217,4 +165,140 @@ func archiveAndRemove(dir string, pm core.PrivilegeManager) error {
 		"-rf",
 		dir,
 	})
+}
+
+func manageUsers(pm core.PrivilegeManager) error {
+	infoStyle := lipgloss.NewStyle().Foreground(lipgloss.Blue)
+	warnStyle := lipgloss.NewStyle().Foreground(lipgloss.Yellow)
+
+	fmt.Println(infoStyle.Render("Adding users:"))
+
+	users, err := core.GetUserConfigs()
+	if err != nil {
+		return err
+	}
+
+	userList, homes, err := core.GetUsers()
+	if err != nil {
+		return err
+	}
+
+	var cfgUsers []string
+
+	for _, userCfg := range users {
+		cfgUsers = append(cfgUsers, userCfg.Username)
+
+		if !slices.Contains(userList, userCfg.Username) {
+			fmt.Printf(infoStyle.Render(" - ", userCfg.Fullname))
+
+			args := []string{
+				"-d",
+				userCfg.HomeDir,
+				"-c",
+				userCfg.Fullname,
+				"-s",
+				userCfg.Shell,
+			}
+
+			homeDirArchive := strings.TrimSuffix(userCfg.HomeDir, "/") + ".tar.zst"
+
+			existingHome, err := core.PathExists(
+				homeDirArchive,
+			)
+			if err != nil {
+				return err
+			}
+
+			if existingHome {
+				fmt.Printf(warnStyle.Italic(true).Render(" (restoring)\n"))
+				pm.RunAsAuthUser("tar", []string{"--zstd", "-xvf", homeDirArchive})
+			} else {
+				fmt.Printf("\n")
+				args = append(args, "-m")
+			}
+
+			if len(userCfg.Groups) > 0 {
+				args = append(args, "-G", strings.Join(userCfg.Groups, ","))
+			}
+
+			args = append(args, userCfg.Username)
+
+			if err := pm.RunAsAuthUser("useradd", args); err != nil {
+				return err
+			}
+		}
+	}
+
+	fmt.Println()
+	fmt.Println(infoStyle.Render("Removing users:"))
+
+	for idx, existingUser := range userList {
+		if !slices.Contains(cfgUsers, existingUser) {
+			fmt.Println(infoStyle.Render(" - ", existingUser))
+
+			pm.RunAsAuthUser("userdel", []string{existingUser})
+			archiveAndRemove(homes[idx], pm)
+		}
+	}
+
+	return nil
+}
+
+func installPkgs(pm core.PrivilegeManager) error {
+	args := []string{"-r $NEST_GEN_ROOT/bin", "-b $NEST_GEN_ROOT/db", "-S"}
+	pkglistPath := filepath.Join(os.Getenv("NEST_GEN_ROOT"), "pkglist.scsv")
+	autogenPkglistPath := filepath.Join(os.Getenv("NEST_GEN_ROOT"), "pkglist.scsv")
+
+	var pkglist scsv.KeyValuePairs
+	var autogenPkglist scsv.KeyValuePairs
+
+	pkglistExists, err := core.PathExists(pkglistPath)
+	if err != nil {
+		return err
+	}
+
+	autogenPkglistExists, err := core.PathExists(autogenPkglistPath)
+	if err != nil {
+		return err
+	}
+
+	if pkglistExists {
+		pkglist, err = scsv.ParseFile(pkglistPath)
+		if err != nil {
+			return err
+		}
+	}
+
+	if autogenPkglistExists {
+		autogenPkglist, err = scsv.ParseFile(autogenPkglistPath)
+		if err != nil {
+			return err
+		}
+	}
+
+	var pkgs []string
+
+	for repo := range pkglist {
+		for _, pkg := range pkglist[repo] {
+			pkgs = append(pkgs, pkg[0])
+		}
+	}
+
+	for repo := range autogenPkglist {
+		for _, pkg := range autogenPkglist[repo] {
+			if slices.Contains(pkgs, pkg[0]) {
+				continue
+			}
+
+			pkgs = append(pkgs, pkg[0])
+		}
+	}
+
+	args = append(args, pkgs...)
+
+	if err := pm.RunAsAuthUser("pacman", args); err != nil {
+		return err
+	}
+
+	return nil
 }
